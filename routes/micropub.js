1afa3f554c5c06647672bed7f1921fe745df8662
Bret <bcomnes@gmail.com>
2014-08-04 10:10:22 -0700

    Beginnings of the micropub endpoint.

--- routes/micropub.js
+++ routes/micropub.js
@@ -1,36 +1,41 @@
 // Micropub route
 var express = require('express');
 var router = express.Router();
+var url = require('url');
 
 var authorize = require('../lib/authorize');
 var multiParse = require('../lib/multiParse');
-var handleFiles = require('../lib/handleFiles')
+var handleFiles = require('../lib/handleFiles');
 var stagePost = require('../lib/stagePost');
-//var publishPost = require('../lib/publishPost);
+var publishPost = require('../lib/publishPost');
 
-/* GET home page. */
+var settings = require('../settings');
+
+
+
+/* GET micropub landing page */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Gitpub µPub Endpoint' });
 });
 
-//router.post('/',  authorize,
-//                  multiParse,
-//                  stagePost,
-//                  publishPost,
-//                  function(res, req) {
-//                    // Report sucessful posting here.
-//                  });
-//
+/* POST micropub */
+router.post('/',  authorize,
+                  multiParse,
+                  stagePost,
+                  publishPost,
+                  function(res, req) {
+                    // Report sucessful posting here.
+                  });
 
+/* GET Test Form */
 router.get('/post', function(req, res) {
   res.render('post');
 });
 
-router.post('/post',authorize,multiParse,stagePost, function(req, res) {
-  if (req.body) console.log(req.body);
-  if (req.token) console.log(req.token);
-  if (req.files) console.log(req.files);
-  res.redirect('post');
+/* POST Test form */
+router.post('/post',authorize,multiParse,stagePost,publishPost, function(req, res) {
+  res.set('Location', url.parse([settings.domain + req.slug].join('')));
+  res.status(201).send('Created Post at ' + [settings.domain + req.slug].join(''));
 })
 
 module.exports = router;
