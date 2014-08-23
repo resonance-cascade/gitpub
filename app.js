872b2715a4be43eb3b42510b0ad024d8e6c249c1
Bret Comnes <bret@Bret-MBR.local>
2014-08-23 15:17:37 -0700

    Fixed depreciation warnings.  no favicon till we get a favicon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');  // Does not handle multipart
 var busboy =  require('connect-busboy');
@@ -13,10 +13,10 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(favicon());
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(busboy());
 app.use(express.static(path.join(__dirname, 'public')));
 
