62238382d5d5ed1b8ea39db389b953b5615f0ab7
Bret Comnes <bcomnes@gmail.com>
2014-09-22 12:53:07 -0700

    Wrote csvParse route

--- test/csvParse.js
+++ test/csvParse.js
@@ -1,21 +1,46 @@
 var assert = require('assert');
-var formEncoder = require('form-urlencoded');
-
 var csvParse = require('../lib/csvParse');
 
 describe('The csvParse route', function() {
   var req = {};
   var res = {};
   req.body = {};
+
+  afterEach(function() {
+    // Clear the req.body object.
+    req.body = {};
+  });
+
   describe('with populated csv parameters', function() {
-    before(function(next) {
-      req.body.syndication = "http://instagram.com/p/s80DrsRfjP,https://twitter.com/bretolius/status/513150506185666560"
-      req.body['syndicate-to'] = "flickr.com/people/bretc,plus.google.com/+BretComnes"
-      csvParse(req,res,next)
+
+    before(function() {
+      req.body.syndication = "hey,hi,heep"
+      req.body['syndicate-to'] = "bleep,bloop,blop"
     });
-    it ('should result in two arrays', function() {
-      assert(req.body.syndication == ["http://instagram.com/p/s80DrsRfjP", "https://twitter.com/bretolius/status/513150506185666560"])
-      assert(req.body['syndicate-to'] == ["flickr.com/people/bretc", "plus.google.com/+BretComnes"])
+
+    it ('should result in two arrays', function(done) {
+      csvParse(req,res,function(err) {
+        assert(req.body.syndication instanceof Array)
+        assert(req.body['syndicate-to'] instanceof Array)
+        done();
+      });
     })
+
   })
+
+  describe('with an empty paramter', function() {
+    before(function() {
+      req.body.syndication = "hey,hi,heep"
+    });
+
+    it('should result in one arrray and one undefined value', function(done) {
+      csvParse(req,res, function(err) {
+        assert(req.body.syndication instanceof Array)
+        assert(typeof req.body['syndicate-to'] === "undefined")
+        done();
+      })
+    })
+
+  })
+
 })
