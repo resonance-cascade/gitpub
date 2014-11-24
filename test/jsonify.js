cc44863899ac5590acd0d0190bd412247b068813
Bret Comnes <bcomnes@gmail.com>
2014-11-24 15:47:49 -0800

    Added more micropub request parsing :(

--- test/jsonify.js
+++ test/jsonify.js
@@ -15,11 +15,11 @@ test('The jsonify middleware', function(t) {
 
   t.test('with populated arrays', function(t) {
 
-    t.plan(9);
+    t.plan(10);
 
     init();
     req.body.syndication = "hey,hi,heep";
-    req.body['syndicate-to'] = "bleep,bloop,blop";
+    req.body['syndicate-to'] = "https://twitter.com/bretolius/status/526580189115863041,http://instagram.com/p/toorXxxfoq/,http://news.indiewebcamp.com/post/bret.io/2013/06/28/indiewebcamp-2013-roundup/";
     req.body.category = "camera,instagram,photo,athing";
     req.tokenData.scope = 'post';
 
@@ -33,6 +33,17 @@ test('The jsonify middleware', function(t) {
       t.equal(req.body.category.length, 4, 'category array should be length 4')
       t.assert(req.tokenData.scope instanceof Array, 'scope should be an array')
       t.equal(req.tokenData.scope.length, 1, 'scope array should be length 1')
+      var expected = [{
+        name: 'twitter.com',
+        url: 'https://twitter.com/bretolius/status/526580189115863041'
+      }, {
+        name: 'instagram.com',
+        url: 'http://instagram.com/p/toorXxxfoq/'
+      }, {
+        name: 'news.indiewebcamp.com',
+        url: 'http://news.indiewebcamp.com/post/bret.io/2013/06/28/indiewebcamp-2013-roundup/'
+      }]
+      t.assert(req.body['syndicate-to'][0].url && req.body['syndicate-to'][0].name, 'syndicated-to should have names and urls')
     })
   })
 
